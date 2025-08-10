a1b5e6cfcebcdb4581b9476df36229f37dc1647e
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-08-10 23:48:25 +0300

    V01-V02

--- Program.cs
+++ Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Collections : Built-in DS in C#
+            //Array : Container
+
+            //Collections in C# 
+            //Lists
+            //Hashtable
+
+            //Generic - Non Generic
+            //Lists : 
+            //Non-Generic Lists : ArrayList- stack - Queue
+            //Generic Lists : list,LinkedList , stack , Queue
+
+            //Hashtables :
+            //Non-Generic HashTable :hashtable
+            //generic Hashtable:Dictionary - sorteddectionary
+
         }
     }
 }
