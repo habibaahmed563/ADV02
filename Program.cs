3c6ff07fa489d0e7933467bcd62a15923ea0f419
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-08-12 05:06:26 +0300

    Q2

--- Program.cs
+++ Program.cs
@@ -7,33 +7,63 @@
 
             // Q1 :
 
-            string[] firstLine = Console.ReadLine().Split();
-            int N = int.Parse(firstLine[0]);
-            int Q = int.Parse(firstLine[1]);
+            //string[] firstLine = Console.ReadLine().Split();
+            //int N = int.Parse(firstLine[0]);
+            //int Q = int.Parse(firstLine[1]);
 
-            string[] array = Console.ReadLine().Split();
-            int[] arr = new int[N];
+            //string[] array = Console.ReadLine().Split();
+            //int[] arr = new int[N];
 
-            for(int i=0; i<N ; i++)
-            {
-                arr[i] = int.Parse(array[i]);
-            }
+            //for(int i=0; i<N ; i++)
+            //{
+            //    arr[i] = int.Parse(array[i]);
+            //}
 
-            for (int i = 0; i < Q; i++)
-            {
-                int count = 0;
-                int x = int.Parse(Console.ReadLine());
-                
-                for(int j=0; j<N; j++)
-                {
-                    if(arr[j] > x)
-                    {
-                        count++;
-                    }
-                }
-                Console.WriteLine(count);
-            }
+            //for (int i = 0; i < Q; i++)
+            //{
+            //    int count = 0;
+            //    int x = int.Parse(Console.ReadLine());
 
+            //    for(int j=0; j<N; j++)
+            //    {
+            //        if(arr[j] > x)
+            //        {
+            //            count++;
+            //        }
+            //    }
+            //    Console.WriteLine(count);
+            //}
+
+
+
+            // Q2 : 
+
+
+            //int N = int.Parse(Console.ReadLine());
+
+            //string[] input = Console.ReadLine().Split();
+            //int[] arr = new int[N];
+
+            //for(int i=0; i<N; i++)
+            //{
+            //    arr[i] = int.Parse(input[i]);
+            //}
+
+            //bool ispalindrome = true;
+            //for(int i =0; i<N/2; i++)
+            //{
+            //    if (arr[i]!= arr[N-1-i])
+            //    {
+            //        ispalindrome = false;
+            //        break;
+            //    }
+            //}
+            //if (ispalindrome)
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //    Console.WriteLine("No");
 
 
         }
