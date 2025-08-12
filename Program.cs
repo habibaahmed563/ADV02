3d2c25ce25ad6106886363c7b2331ecb8775a868
habibaahmedabdelmalek@outlook.sa <habibaahmedabdelmalek@outlook.sa>
2025-08-12 04:58:00 +0300

    Q1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Q1 :
+
+            string[] firstLine = Console.ReadLine().Split();
+            int N = int.Parse(firstLine[0]);
+            int Q = int.Parse(firstLine[1]);
+
+            string[] array = Console.ReadLine().Split();
+            int[] arr = new int[N];
+
+            for(int i=0; i<N ; i++)
+            {
+                arr[i] = int.Parse(array[i]);
+            }
+
+            for (int i = 0; i < Q; i++)
+            {
+                int count = 0;
+                int x = int.Parse(Console.ReadLine());
+                
+                for(int j=0; j<N; j++)
+                {
+                    if(arr[j] > x)
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+            }
+
+
+
         }
     }
 }
